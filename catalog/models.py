9073354cf0ecd4024b6e1f27e7625b2340feb0c7
Chris <alexanderchris1475@yahoo.com>
2017-11-16 16:57:29 -0500

    edited models.py

--- catalog/models.py
+++ catalog/models.py
@@ -1,61 +1,66 @@
+"""
+SQLAlchemy database model for the application.
+"""
 from sqlalchemy import Column, ForeignKey, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import relationship, backref
+from sqlalchemy.orm import relationship
 
 
 Base = declarative_base()
 
 
 class User(Base):
+    """ Web site user registration model """
+
     __tablename__ = 'user'
 
-    id = Column(Integer, primary_key = True)
-    name = Column(String(250), nullable = False)
-    email = Column(String(250), nullable = False)
-    picture = Column(String(250))
-    categories = relationship('Category', cascade='all, delete-orphan', backref = 'user')
-    items = relationship('Items', cascade='all, delete-orphan', backref = 'user')
-
-class Category(Base):
-    __tablename__ = 'category'
-   
     id = Column(Integer, primary_key=True)
     name = Column(String(250), nullable=False)
+    email = Column(String(250), nullable=False, unique=True)
+    picture = Column(String(250))
+
+
+class Category(Base):
+    """ Category model """
+
+    __tablename__ = 'category'
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String(250), nullable=False, unique=True)
     user_id = Column(Integer, ForeignKey('user.id'))
-    user = relationship('User', backref=backref('categories', cascade='all, delete'))
-    items = relationship('Items', cascade='all, delete-orphan', backref = 'category')
+    user = relationship(User)
+    items = relationship("Items", cascade="all,delete", backref="category")
+
     @property
     def serialize(self):
-       """Return object data in easily serializeable format"""
-       return {
-           'name'         : self.name,
-           'id'           : self.id,
-       }
- 
-class Items(Base):
-    __tablename__ = 'items'
+        return {
+            'id': self.id,
+            'name': self.name,
+            'user_id': self.user_id,
+            'items': [i.serialize for i in self.items]
+        }
 
-    id = Column(Integer, primary_key = True)
-    name = Column(String(250), nullable = False, unique = True)
+
+class Items(Base):
+    """ Item model """
+
+    __tablename__ = 'item'
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String(250), nullable=False, unique=True)
     description = Column(String(1000))
-    price = Column(String(8))
     image = Column(String(250))
     category_id = Column(Integer, ForeignKey('category.id'))
-    category = relationship('Category', backref=backref('category', cascade='all, delete'))
     user_id = Column(Integer, ForeignKey('user.id'))
-    user = relationship('User', backref=backref('items', cascade='all, delete'))
-
+    user = relationship(User)
 
     @property
     def serialize(self):
-       """Return object data in easily serializeable format"""
-       return {
-           'name'         : self.name,
-           'description'  : self.description,
-           'id'           : self.id,
-           'user_id': self.user_id,
-           'category_id': self.category_id,           
-           'price'        : self.price,
-           'image'        : self.image,
-       }
-
+        return {
+            'name': self.name,
+            'description': self.description,
+            'id': self.id,
+            'user_id': self.user_id,
+            'category_id': self.category_id,
+            'image': self.image
+}
